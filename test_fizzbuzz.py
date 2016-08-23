471f8a0db8713930e5398ba826f8b3ee7a53caa8
toby676 <tjcmui@gmail.com>
2016-08-23 15:43:09 +0100

    test for fizz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,5 +6,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_1_returns_1(self):
         self.assertEqual(FizzBuzz.play(1), 1)
 
+    def test_3_returns_fizz(self):
+        self.assertEqual(FizzBuzz.play(3), "Fizz")
+
 if __name__ == '__main__':
 	unittest.main()
